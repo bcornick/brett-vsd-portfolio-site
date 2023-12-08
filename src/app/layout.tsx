d236dfba09613e70d1046b7fdf0fbd7cb335cf73
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2023-12-08 01:02:10 -0700

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,43 @@ export const metadata: Metadata = {
     template: '%s - Brett Cornick',
     default: 'Brett Cornick',
   },
+  openGraph: {
+    title: 'Brett Cornick',
+    description:
+      'Brett is a climate-first technologist with a strong foundation in materials science, process design, software development, and AI/automation.',
+    url: 'https://brettcornick.com',
+    siteName: 'Brett Cornick Portfolio',
+    images: [
+      {
+        url: 'https://imgur.com/a/xoSXl2F',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Brett Cornick',
+    description:
+      'Brett is a climate-first technologist with a strong foundation in materials science, process design, software development, and AI/automation.',
+    creator: '@brett_cornick',
+    images: ['https://imgur.com/a/xoSXl2F'],
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {

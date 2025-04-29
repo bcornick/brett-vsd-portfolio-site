85b6a76c8c0307600286da9c723228ad4f589ea3
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2025-04-29 15:01:55 -0600

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { type Metadata } from 'next'
 
 import { RootLayout } from '@/components/RootLayout'
 
+import { Analytics } from '@vercel/analytics/next'
+
 import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
@@ -46,6 +48,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
+        <Analytics />
       </body>
     </html>
   )
